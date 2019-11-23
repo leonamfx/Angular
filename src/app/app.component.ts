71866e8c4231edd72c025c03fe597fd5477ac949
LeonamLima <leonamfx@gmail.com>
2019-11-23 10:58:52 -0300

    Exemplo 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,19 @@ export class AppComponent {
   title = 'ExercicioAngular'
   nomes: string [] = ['joão', 'maria', 'josé','pedro','felipe','carlos'];
   nomesFiltro: string[];
-
+  pessoas: any = [
+    {id: 1, nome: 'joão', salario: 5000},
+    {id: 2, nome: 'maria', salario: 1000},
+    {id: 3, nome: 'josé', salario: 2000},
+    {id: 4, nome: 'pedro', salario: 3000},
+    {id: 5, nome: 'felipe', salario: 10000},
+    {id: 6, nome: 'carlos', salario: 800},
+  ]
+  getValorTotal(): Number {
+    return this.pessoas.reduce(
+      (soma,pessoa) => soma + pessoa.salario,0);
+    
+  }
   buscar(valor: string){
     this.nomesFiltro = [];
 
