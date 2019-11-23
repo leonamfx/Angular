9672779a44f8a738d81f0d9f96630755aa07c9e5
LeonamLima <leonamfx@gmail.com>
2019-11-23 10:28:25 -0300

    Exemplo 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ExercicioAngular'
+  nomes: string [] = ['joão', 'maria', 'josé','pedro','felipe','carlos'];
+
+}
