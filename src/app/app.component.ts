312c4ce2e3a87440880ee2124cd00982432c24ae
LeonamLima <leonamfx@gmail.com>
2019-11-23 10:42:22 -0300

    Exemplo 5 Metodo 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ExercicioAngular'
   nomes: string [] = ['joão', 'maria', 'josé','pedro','felipe','carlos'];
-  
+  nomesFiltro: string[];
+
   buscar(valor: string){
-    alert(valor)
+    this.nomesFiltro = [];
+
+    //método 1
+    for (var i = 0; i < this.nomes.length; i++) {
+      if(this.nomes[i].toLowerCase().includes(valor.toLowerCase())) {
+        this.nomesFiltro.push(this.nomes[i]);
+      }
+    }
+    
   }
 
 }
